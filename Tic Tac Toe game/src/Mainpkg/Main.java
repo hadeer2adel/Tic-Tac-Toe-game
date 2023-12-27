1bf7c290654bf01578ec5820f25dc5423ad20546
Hadeer <hadeeradel502@gmail.com>
2023-12-28 01:05:46 +0200

    Add branch dev

--- Tic Tac Toe game/src/Mainpkg/Main.java
+++ Tic Tac Toe game/src/Mainpkg/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    
     public static void main(String[] args) {
         launch(args);
     }
