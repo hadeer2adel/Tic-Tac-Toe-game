b162e8dd17444d0489bae2d789a317d72eacc0ee
Aya Mashaly <ayamashaly@gmail.com>
2024-01-01 19:15:22 +0200

    implement Video_screen

--- Tic Tac Toe game/src/Screens/Video_ScreenController.java
+++ Tic Tac Toe game/src/Screens/Video_ScreenController.java
@@ -5,9 +5,21 @@
  */
 package Screens;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,13 +27,44 @@ import javafx.fxml.Initializable;
  * @author Dell
  */
 public class Video_ScreenController implements Initializable {
+    private Stage stage;
+     private Scene scene;
+     private Parent root;
+     
+  
 
-    /**
-     * Initializes the controller class.
-     */
+@FXML
+private MediaView mediaView;
+@FXML
+private File file;
+private Media media;
+private MediaPlayer mediaPlayer;
+  
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        file = new File("losser.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setAutoPlay(true);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
     }    
-    
+
+      public void switchToHome(ActionEvent event) throws IOException{//Home Screen
+        root = FXMLLoader.load(getClass().getResource("/Screens/Home_Screen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+}
+       public void switchToGameAgain(ActionEvent event) throws IOException{//Game Screen
+        root = FXMLLoader.load(getClass().getResource("/Screens/Game_Screen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+}
 }
