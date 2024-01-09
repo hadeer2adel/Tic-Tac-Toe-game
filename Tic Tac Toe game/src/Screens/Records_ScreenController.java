f61af6925a09f47712704962c5eaaa984a5a56bb
sara sobhy <sarasobhy835@gmail.com>
2024-01-09 18:23:20 +0200

    link screen the rest

--- Tic Tac Toe game/src/Screens/Records_ScreenController.java
+++ Tic Tac Toe game/src/Screens/Records_ScreenController.java
@@ -1,27 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Screens;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Ahmed KH
- */
-public class Records_ScreenController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+public class Records_ScreenController{
+
+     private Stage stage;
+     private Scene scene;
+     private Parent root;
+    
+    public void switchToProfile(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/Screens/PlayerInfo_Screen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
