3aeb048ccfb178b20c6fa163bcd8f9100e9ff7a3
Mohame <kotbm055@gmail.com>
2024-01-16 16:43:56 +0200

    create Online_Game_Screen FXML

---
+++ Tic Tac Toe game/src/Screens/Online_Game_ScreenController.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Screens;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author win 10
+ */
+public class Online_Game_ScreenController implements Initializable {
+
+    @FXML
+    private StackPane stackPane1;
+    @FXML
+    private AnchorPane anchorPane1;
+    @FXML
+    private ImageView imag1;
+    @FXML
+    private ImageView imag2;
+    @FXML
+    private ImageView imag3;
+    @FXML
+    private Button btn_back;
+    @FXML
+    private Button btn_record;
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btn8;
+    @FXML
+    private Button btn9;
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label3;
+    @FXML
+    private Label label4;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void switchTo2playermode(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn1(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn2(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn3(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn4(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn5(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn6(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn7(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn8(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBtn9(ActionEvent event) {
+    }
+    
+}
